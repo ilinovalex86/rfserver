4229f94d608d7d1d9af16d1458f5bc1092e73099
Alexej <ilinov86@yandex.ru>
2022-02-09 10:00:40 +0300

    add stream

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module rfserver
 go 1.13
 
 require (
-	github.com/ilinovalex86/tcpserver v1.1.12
+	github.com/ilinovalex86/tcpserver v1.2.0
 	github.com/ilinovalex86/tcpserverweb v1.0.6
 )
--- main.go
+++ main.go
@@ -9,7 +9,15 @@ import (
 )
 
 const tcpServer = "ipAddress:port"
+const streamServer = "ipAddress:port"
 const webServer = "ipAddress:port"
+const boundaryWord = "MJPEGBOUNDARY"
+const headerf = "\r\n" +
+	"--" + boundaryWord + "\r\n" +
+	"Content-Type: image/jpeg\r\n" +
+	"Content-Length: %d\r\n" +
+	"X-Timestamp: 0.000000\r\n" +
+	"\r\n"
 
 //Привязывает tcp клиента к web клиенту
 func auth(w http.ResponseWriter, r *http.Request) {
@@ -59,7 +67,7 @@ func index(w http.ResponseWriter, r *http.Request) {
 			webR.Error = "Компьютер не подключен."
 			web.ToBrowser(w, htmlFile, webR)
 		}
-		http.Redirect(w, r, "/", http.StatusFound)
+		return
 	}
 	webR.Menu = res["nav"]
 	webR.Dirs = res["dirs"]
@@ -81,6 +89,7 @@ func sendFile(w http.ResponseWriter, r *http.Request) {
 	res, err := tcp.Clients.File(id, path)
 	if err != nil {
 		http.Redirect(w, r, "/", http.StatusFound)
+		return
 	}
 	w.Header().Set("Content-Disposition", "attachment; filename="+name)
 	w.Header().Set("Content-Type", "application/octet-stream")
@@ -92,10 +101,50 @@ func logout(w http.ResponseWriter, r *http.Request) {
 	web.ClientClear(w, r)
 }
 
+// Запускает стрим рабочего стола клиента
+func stream(w http.ResponseWriter, r *http.Request) {
+	id, valid := web.Valid(r)
+	if !valid {
+		web.ClientClear(w, r)
+		return
+	}
+	cookie, err := r.Cookie("SessionId")
+	webId := cookie.Value
+	s, err := tcp.Clients.Stream(id, webId)
+	if err != nil {
+		var webR web.Response
+		webR.User = tcp.Clients.User(id)
+		htmlFile := "./html/error.html"
+		webR.Error = fmt.Sprint(err)
+		web.ToBrowser(w, htmlFile, webR)
+		return
+	}
+	imgIndex := 0
+	w.Header().Add("Content-Type", "multipart/x-mixed-replace;boundary="+boundaryWord)
+	for {
+		i, img, err := s.Next(imgIndex)
+		if err != nil {
+			break
+		}
+		header := fmt.Sprintf(headerf, len(img))
+		frame := make([]byte, len(img)+len(header))
+		copy(frame, header)
+		copy(frame[len(header):], img)
+		_, err = w.Write(frame)
+		if err != nil {
+			break
+		}
+		imgIndex = i
+	}
+	s.Remove(webId)
+}
+
 func main() {
-	go tcp.Listen(tcpServer)
+	go tcp.TcpServer(tcpServer)
+	go tcp.StreamServer(streamServer)
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", index)
+	mux.HandleFunc("/stream", stream)
 	mux.HandleFunc("/file", sendFile)
 	mux.HandleFunc("/login", login)
 	mux.HandleFunc("/auth", auth)
@@ -105,4 +154,3 @@ func main() {
 	err := http.ListenAndServe(webServer, mux)
 	log.Fatal(err)
 }
-
